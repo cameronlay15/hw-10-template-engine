245150667fd1bc68d864297d18aa98f2cf2c5282
Cameron Lay <cameronlay15@gmail.com>
2020-09-29 16:34:59 -0400

    passed test 3

--- develop/lib/Intern.js
+++ develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+Employee = require("../lib/Employee.js");
+
+class Intern extends Employee{
+  constructor(name, id, email, school){
+    super(name, id, email);
+    this.school = school;
+  }
+  getRole(){
+    return "Intern"
+  }
+  getSchool(){
+    return this.school;
+  }
+}
+
+// ✓ Can set school via constructor (3ms)
+// ✓ getRole() should return "Intern" (1ms)
+// ✓ Can get school via getSchool()
+
+module.exports = Intern;
