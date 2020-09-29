b9690583b76f0eb9a3eff9194bf739d6bd2b4e5f
Cameron Lay <cameronlay15@gmail.com>
2020-09-29 16:27:10 -0400

    passed 2nd test

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+Employee = require("../lib/Employee.js");
+class Engineer extends Employee{
+  constructor(name, id, email, github){
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole(){
+    return "Engineer"
+  }
+  getGithub(){
+    return this.github;
+  }
+}
+
+// ✓ Can set GitHUb account via constructor (3ms)
+// ✓ getRole() should return "Engineer"
+// ✓ Can get GitHub username via getGithub() (1ms)
+
+module.exports = Engineer;
