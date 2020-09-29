dacc244b8123e3b76b82f3e859f5c5725217ccff
Cameron Lay <cameronlay15@gmail.com>
2020-09-29 17:09:15 -0400

    new employee prompts

--- develop/app.js
+++ develop/app.js
@@ -68,6 +68,61 @@ const internQuestions = () => {
           team.push(intern);
           addMore();
       })
+
+      const mngrQuestions = () => {
+        inquirer.prompt([
+          {
+          type: "input",
+          name: "name",
+          message: "Manager's name?"
+          },
+          {
+          type: "input",
+          name: "id",
+          message: "Manager's ID?"
+          },
+          {
+          type: "input",
+          name: "email",
+          message: "Manager's email?"
+          },
+          {
+          type: "input",
+          name: "officeNumber",
+          message: "Manager's office number?"
+          },
+          ]).then(answer => {
+            const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+              team.push(manager);
+                addMore();
+            })
+      }
+      
+      const addMore = () => {
+        inquirer.prompt({
+          type: "confirm",
+          name: "addMore",
+          message: "Add another employee?"
+        }).then(answer => {
+            if (answer.addMore) {
+              newEmployee();
+              } 
+              else {
+                let html = render(team);
+                  fs.writeFile(outputPath, html, function (err) {
+                    if (err) throw err;
+                    console.log("Employee(s) added.");
+                  });
+              };
+          })
+      }
+      const newEmployee = () => {
+        inquirer.prompt({
+          type: "list",
+          name: "employeeType",
+          message: "Which employee type are you adding?",
+          choices: ["Engineer", "Intern", "Manager"]
+        })  
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
